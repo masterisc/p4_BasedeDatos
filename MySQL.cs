efe0cfac73a3159722ba4b5541279e3a6f6fb332
masterisc <ischardware@hotmail.com>
2015-04-15 09:15:43 -0500

    Agenda en Base de datos

---
+++ MySQL.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+
+namespace BasedeDatos
+{
+
+	public class MySQL
+	{
+        protected MySqlConnection myConnection;
+
+        public MySQL()
+        {
+        }
+
+        protected void abrirConexion()
+        {
+            string connectionString =
+            "Server=localhost;" +
+            "Database=AGENDA;" +
+            "User ID=root;" +
+            "Password=mora;" +
+            "Pooling=false;";
+            this.myConnection = new MySqlConnection(connectionString);
+            this.myConnection.Open();
+        }
+        protected void cerrarConexion()
+        {
+            this.myConnection.Close();
+            this.myConnection = null;
+        }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+namespace BasedeDatos
+{
+	class BaseDeDatos
+		
+	{
+		public BaseDeDatos(){
+			
+		}
+			bool salir= false;
+			
+			
+		public void menu()
+        {
+		
+            Contacto contacto = new Contacto();
+            int opcion;
+            string id, nombre, telefono;
+            
+            Console.Write("\n1.- Ver todos los contactos"+
+                "\n2.- Agregar contacto"+
+                "\n3.- Editar contacto"+
+                "\n4.- Eliminar contacto"+
+                "\n5.- Salir"+
+                "\n\n\n\tElige una opcion: ");
+            try
+            {
+                opcion = int.Parse(Console.ReadLine());
+                switch (opcion)
+                {
+                    case 1: contacto.mostrarTodos();
+                        break;
+                    case 2: Console.Write("nombre: ");
+                        nombre = Console.ReadLine();
+                        Console.Write("telefono: ");
+                        telefono = Console.ReadLine();
+                        contacto.insertarRegistroNuevo(nombre, telefono);
+                        break;
+                    case 3: Console.Write("id: ");
+                        id = Console.ReadLine();
+                        if (contacto.consultaId(id).Length > 0)
+                        {
+                            Console.Write("nombre: ");
+                            nombre = Console.ReadLine();
+                            Console.Write("telefono: ");
+                            telefono = Console.ReadLine();
+                            contacto.editarRegistro(id, nombre, telefono);
+                        }
+                        else
+                            Console.WriteLine("El id no existe");
+                        break;
+                    case 4: Console.Write("id: ");
+                        id = Console.ReadLine();
+                        if (contacto.consultaId(id).Length > 0)
+                        {
+                            Console.WriteLine("Contacto eliminado");
+                            contacto.eliminarRegistroPorId(id);
+                        }
+                        else
+                            Console.WriteLine("El id no existe");
+                        break;
+                    case 5: Console.WriteLine("Gracias por utilizar el programa!!!");
+                        salir = true;
+                        break;
+                    default: Console.WriteLine("Opcion invalida");
+                        break;
+                }
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("ingresa un entero");
+            }
+
+        }
+
+
+        static void Main(string[] args)
+        {
+            
+            Console.Title = "Base de datos";
+            Console.BackgroundColor = ConsoleColor.Gray;
+            Console.ForegroundColor = ConsoleColor.Blue;
+            
+            BaseDeDatos principal = new BaseDeDatos();
+            do
+            {
+                Console.Clear();
+                principal.menu();
+                Console.ReadKey();
+            } while (!principal.salir);
+        }
+	}
+}
